e78f209f7b4060430270e92e022983e41d2a6726
Yorye <jorge.abrodes@gmail.com>
2025-06-25 22:25:05 +0200

    (002 - [Fearure] - rear esqueleto inicial de carpetas y archivos para ForgeFS)

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,22 @@ use axum::{
     routing::get,
     Router,
 };
+use dotenv::dotenv;
+use std::env;
 
 #[tokio::main]
 async fn main() {
+    
+    dotenv().ok();
+
+    let url = env::var("URL").expect("Error en .env falta URL");
+    let project_name = env::var("PROJECT_NAME").expect("Error en .env falta PROJECT_NAMe");
+
+    println!("{} corriendo en: {}", project_name, url);
     // build our application with a single route
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind(url).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
--- src/routes/router.rs
+++
@@ -1,7 +0,0 @@
-use axum::{Router, routing::get};
-
-// our router
-let app = Router::new()
-    .route("/", get(root));
-// which calls one of these handlers
-async fn root() {}
