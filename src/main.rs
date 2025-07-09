f02a4b49ad257a455507a043a3f135b402f72ca1
Yorye <jorge.abrodes@gmail.com>
2025-07-09 11:10:47 +0200

    Refactor main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ mod handlers;
 mod services;
 //mod infra;
 
-use axum;
 use dotenv::dotenv;
 use std::env;
 
@@ -11,17 +10,14 @@ use std::env;
 async fn main() {
     
     dotenv().ok();
-
+    
     let url = env::var("URL").expect("Error en .env falta URL");
     let project_name = env::var("PROJECT_NAME").expect("Error en .env falta PROJECT_NAMe");
-
-    //handlers::files::pintamos_en_pantalla();
-
+    
     println!("{} corriendo en: {}", project_name, url);
-    // build our application with a single routes
-
+    
     let app = routes::app_routes();
-    // run our app with hyper, listening globally on port 3000
+    
     let listener = tokio::net::TcpListener::bind(url).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
